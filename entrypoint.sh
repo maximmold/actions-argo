428a30748e81bd87e69675b24d1d09d894cf19fe
Maxim Moldenhauer <moldenhauer_maxim@bah.com>
2020-01-24 13:13:17 -0500

    Include github branch as parameter to argo cli

--- entrypoint.sh
+++ entrypoint.sh
@@ -84,8 +84,10 @@ else
     NAMESPACE_CMD=""
 fi
 
+GITHUB_BRANCH=[[ ! -z "$GITHUB_HEAD_REF" ]] && echo $GITHUB_HEAD_REF || echo ${GITHUB_REF#refs/heads/}
+
 # Execute the command
-ARGO_CMD="argo submit $INPUT_WORKFLOW_YAML_PATH --name $WORKFLOW_NAME $NAMESPACE_CMD $PARAM_FILE_CMD -p github_branch=$INPUT_GITHUB_BRANCH"
+ARGO_CMD="argo submit $INPUT_WORKFLOW_YAML_PATH --name $WORKFLOW_NAME $NAMESPACE_CMD $PARAM_FILE_CMD -p github_branch=$GITHUB_BRANCH"
 echo "executing command: $ARGO_CMD"
 eval $ARGO_CMD
 
